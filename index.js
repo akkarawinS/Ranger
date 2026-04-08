cf3fcb47ab4f7d195ce494498b161432c28fcb03
AkkarawinS <akkarawinRX@gmail.com>
2026-04-08 14:56:30 +0700

    fix conflith

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import Ranger from "./Ranger.js";
-import Villians from "./Villains.js"
-
-let man1 = new Ranger("stukasa","red","Shark")
-let man2 = new Ranger("josuke","blue","Lion")
-let Gigi1 = new Villians("Googa","Gogre")
-
-man1.transform()
-console.log(man1.combat(Gigi1))
-
-
-
-
-=======
 import Gaored from "./gaored.js";
 import Ranger from "./Ranger.js";
 
@@ -23,4 +8,3 @@ gaored.transform();
 gaored.shout();
 
 gaored.callAnimalSpirit();
->>>>>>> b80946052883538ad3e5004bd29d6928c85d0f5b
